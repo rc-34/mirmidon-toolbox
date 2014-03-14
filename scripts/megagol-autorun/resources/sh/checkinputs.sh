55f89ee000df288a30d7215d070cc9d24a51d45a
Romain <romain.chailan@gmail.com>
2014-03-14 09:01:23 +0100

    [megagol-autorun]  ARPERA* misuse to count files in wd

--- scripts/megagol-autorun/resources/sh/checkinputs.sh
+++ scripts/megagol-autorun/resources/sh/checkinputs.sh
@@ -8,87 +8,87 @@ function checkinputs(){
 	if [ ! -e $WORKINGDIR/ww3_grid ]
 		then
 		log "warning" "$WORKINGDIR/ww3_grid does not exists. Please verify ww3 compilation [and links]."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_prnc ]
 		then
 		log "warning" "$WORKINGDIR/ww3_prnc does not exists. Please verify ww3 compilation [and links]."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_strt ]
 		then
 		log "warning" "$WORKINGDIR/ww3_strt does not exists. Please verify ww3 compilation [and links]."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_shel ]
 		then
 		log "warning" "$WORKINGDIR/ww3_shel does not exists. Please verify ww3 compilation [and links]."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_ounf ]
 		then
 		log "warning" "$WORKINGDIR/ww3_ounf does not exists. Please verify ww3 compilation [and links]."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_ounp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_ounp does not exists. Please verify ww3 compilation [and links]."
-		returnCode=-1
+		returnCode=1
 	fi
 
 	## inp ##
 	if [ ! -e $WORKINGDIR/ww3_grid.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_grid.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_prnc-current.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_prnc-current.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_prnc-wind.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_prnc-wind.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/prnc.sh ]
 		then
 		log "warning" "$WORKINGDIR/prnc.sh does not exists."
-		returnCode=-1
+		returnCode=1
 	else
 		chmod 775 $WORKINGDIR/prnc.sh
 	fi
 	if [ ! -e $WORKINGDIR/ww3_strt.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_strt.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_shel.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_shel.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_ounf.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_ounf.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 	if [ ! -e $WORKINGDIR/ww3_ounp.inp ]
 		then
 		log "warning" "$WORKINGDIR/ww3_ounp.inp does not exists."
-		returnCode=-1
+		returnCode=1
 	fi
 
-	if [ $(ls $WORKINGDIR | grep ARPERA*.nc | wc -l) -eq 0 ]
+	if [ $(ls -l $WORKINGDIR | grep ARPERA | wc -l) -eq 0 ]
 		then
 		log "warning" "No input wind matching ARPERA pattern found"
-		returnCode=-1
+		returnCode=1
 	fi
-	if [ $(ls $WORKINGDIR | grep NM824*.nc | wc -l) -eq 0 ]
+	if [ $(ls -l $WORKINGDIR | grep NM824 | wc -l) -eq 0 ]
 		then
 		log "warning" "No input current matching NM824 pattern found"
-		returnCode=-1
+		returnCode=1
 	fi
 
 	return $returnCode
