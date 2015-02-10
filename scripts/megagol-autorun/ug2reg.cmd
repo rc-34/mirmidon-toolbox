25bb82347486340c696b084b1b1625d374f71fec
Romain <romain.chailan@gmail.com>
2015-02-10 19:18:25 +0100

    [megagolautorun] interpol script modification

--- scripts/megagol-autorun/ug2reg.cmd
+++ scripts/megagol-autorun/ug2reg.cmd
@@ -2,18 +2,14 @@
 # # @ account_no = mirmidon
 # # @ class = intel
 # # @ job_type = serial
-# # @ wall_clock_limit = 03:00:00,2:50:00
+# # @ wall_clock_limit = 12:00:00,11:50:00
 # # @ environment = COPY_ALL
 # # @ queue
 
 # ###ENV###
-#export WORKINGDIR=/scratch/chailanr/mirmidon-toolbox/scripts/megagol-autorun
 export WWATCH3_NETCDF=NC4
 export NETCDF_CONFIG=/work/mirmidon/softs/intel-13.x-soft/io/netcdf/netcdf-fortran-4.1.3/bin/nc-config
 export LD_LIBRARY_PATH=/work/mirmidon/softs/intel-13.x-soft/io/nco/nco-4.4.2/lib:/work/mirmidon/softs/intel-13.x-soft/io/netcdf/netcdf-fortran-4.1.3/lib:$LD_LIBRARY_PATH
-#export PATH=/work/mirmidon/softs/intel-13.x-soft/io/nco/nco-4.4.2/bin:$PATH
-
-# cd $WORKINGDIR
 
 #source utilities
 source ./resources/sh/utility.sh
@@ -32,9 +28,8 @@ beginningyear=$1
 endyear=$2
 interpoldir="resources/interpolresources"
 regoutputsdir="outputs/ounf/REG"
-regoutputsdir="REG"
-megagoloutputsdir="outputs/gridded"
-finidir="outputs"
+megagoloutputsdir="/gpfs2/scratch/chailanr/mirmidon-toolbox/scripts/megagol-autorun/outputs/gridded"
+work="workREG"
 
 if [ ! -d $regoutputsdir ];
 	then
@@ -45,35 +40,44 @@ sequence=$(seq $beginningyear $endyear)
 log $? "Sequence : determined."
 
 for year in $sequence ; do
-	workdir=$finidir/$year
+	workdir=$work/$year
+	if [ ! -d $workdir ]; 
+		then
+		mkdir -p $workdir
+	fi
 	cd $workdir
-	log $? "Change to $year directory"
+	log $? "Change to $workdir directory"
 
 	cp ../../$interpoldir/ww3_gint.inp .
 	cp ../../$interpoldir/ww3_grid.inp.ug .
 	cp ../../$interpoldir/ww3_grid.inp.reg .
 	cp ../../$interpoldir/ww3_ounf.inp.reg .
 	cp ../../$interpoldir/ww3_ounf.inp.ug .
-	cp ../../$interpoldir/big.xy ./big.xy
+	cp ../../$interpoldir/big.xy .
+	cp ../../$interpoldir/ww3_grid . 
+	cp ../../$interpoldir/ww3_ounf . 
+	log $? "cp files from $interpoldir"
+
+	cp $megagoloutputsdir/$year/mesh.msh .
+	log $? "cp files from $megagoloutputsdir/$year"	
 
 	formatinpInterpol ./ $year
 	log $? "Format date in .inp files"
 
 	rm mod_def*
 	ln -sf ww3_grid.inp.ug ww3_grid.inp
-	../../$interpoldir/ww3_grid > grid.out.ug
+	./ww3_grid > grid.out.ug
 	log $? "Re-process ug grid with updated ww3_grid binary"
 	mv mod_def.ww3 mod_def.ug
 
 	ln -sf ww3_grid.inp.reg ww3_grid.inp
-	../../$interpoldir/ww3_grid > grid.out.reg
+	./ww3_grid > grid.out.reg
 	log $? "Process reg grid with updated ww3_grid binary"
 	mv mod_def.ww3 mod_def.reg
 
 	ln -sf mod_def.ug  mod_def.ww3
-	#ln -sf $megagoloutputsdir/$year/out_grd.ww3 out_grd.ug
-	cp $megagoloutputsdir/$year/out_grd.ww3 out_grd.ug
-	log $? "copy out_grd.ww3.ug"
+	ln -s $megagoloutputsdir/$year/out_grd.ww3 out_grd.ug
+	log $? "link out_grd.ww3.ug"
 	rightnow
 	log "notice" "$d"
 
@@ -90,7 +94,7 @@ for year in $sequence ; do
 	rightnow
 	log "notice" "$d"
 
-	mv MEGAGOL2015areg-BIG-OUNF*.nc ../../$regoutputsdir/.
+	mv MEGAGOL2015a-REG-big-OUNF*.nc ../../$regoutputsdir/.
 	log $? "Move interpolated nc files"
 
 	ln -sf ww3_ounf.inp.ug ww3_ounf.inp
